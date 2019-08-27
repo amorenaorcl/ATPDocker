fcbd0822eee6100e9d584fdcc2184abcfe59378d
Andrea Morena <andrea.morena@oracle.com>
2019-08-27 17:24:40 +0100

    Update kubescript.sh

--- kubescript.sh
+++ kubescript.sh
@@ -1,8 +1,8 @@
 export KUBECONFIG=./mykubeconfig
 kubectl config view
 kubectl get nodes
-kubectl delete deployment atp2 --ignore-not-found=true
-kubectl apply -f atp2.yaml
-kubectl get services atp2
+kubectl delete deployment atp1 --ignore-not-found=true
+kubectl apply -f atp1.yaml
+kubectl get services atp1
 kubectl get pods
 kubectl describe pods
